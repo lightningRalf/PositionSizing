e8e1b6001da29aaff872849588ae1ae37aa37724
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-15 20:12:34 +0100

    Update PS_Kelly.py
    corrected formula / now shows correct values

--- PS_Kelly.py
+++ PS_Kelly.py
@@ -1,8 +1,8 @@
 def calculate_rr(entry, tp, sl):
     return abs((tp - entry) / (entry - sl))
 
-def kelly_criterion(win_probability, win_loss_ratio):
-    return win_probability - ((1 - win_probability) / win_loss_ratio)
+def kelly_criterion(win_probability, reward_risk_ratio):
+    return win_probability - ((1 - win_probability) / reward_risk_ratio)
 
 def main():
     trading_stack = float(input("Enter your trading stack: "))
@@ -22,13 +22,18 @@ def main():
             break
 
     kelly_values = [kelly_criterion(win_probability, rr) for entry, tp, sl, rr, win_probability in trades]
-    total_kelly_value = sum(kelly_values)
-    adjusted_trading_sizes = [kv * trading_stack / total_kelly_value for kv in kelly_values]
+    
+    if len(trades) == 1:
+        adjusted_trading_sizes = [kelly_values[0] * trading_stack]
+    else:
+        total_kelly_value = sum(kelly_values)
+        adjusted_trading_sizes = [kv * trading_stack / total_kelly_value for kv in kelly_values]
 
     for i, (entry, tp, sl, rr, win_probability) in enumerate(trades, start=1):
         print(f"\nTrade {i}:")
         print(f"Risk-Reward Ratio: {rr:.2f}")
         print(f"Win Probability: {win_probability * 100:.2f}%")
+        print(f"Kelly Criteria: {kelly_values[i-1]:.2f}")
         print(f"Adjusted Trading Size: {adjusted_trading_sizes[i-1]:.2f}")
 
 if __name__ == "__main__":
