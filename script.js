a236c28dc3d77b0cd9902050b3a90cb804efd140
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-15 09:39:42 +0100

    Update script.js
     The "Calculate" button is not functional because the code to perform calculations and display the results is not included in the provided JavaScript file. You will need to add the relevant calculation code to the event listener for the form's submit event.
    
    In this updated version, I have included the calculate_rr() and kelly_criterion() functions from the previous Python code. The form's submit event now retrieves the entered values, calculates the required values using these functions, and displays the ticker symbol and the adjusted trading size in the results table.

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-function createRow() {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-        <td><input type="text" name="ticker" required></td>
-        <td><input type="number" step="0.01" name="entry" required></td>
-        <td><input type="number" step="0.01" name="tp" required></td>
-        <td><input type="number" step="0.01" name="sl" required></td>
-        <td><input type="number" step="0.01" min="0" max="1" name="winProbability" required></td>
-    `;
-    return row;
+// ... (previous code) ...
+
+function calculate_rr(entry, tp, sl) {
+    return Math.abs((tp - entry) / (entry - sl));
+}
+
+function kelly_criterion(win_probability, win_loss_ratio) {
+    return win_probability - ((1 - win_probability) / win_loss_ratio);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const tradesForm = document.getElementById('trades-form');
-    const tradesInput = document.getElementById('trades-input');
-    const results = document.getElementById('results');
-    const addRowButton = document.getElementById('add-row');
-
-    addRowButton.onclick = (event) => {
-        event.preventDefault();
-        tradesInput.appendChild(createRow());
-    };
+    // ... (previous code) ...
 
     tradesForm.addEventListener('submit', (event) => {
         event.preventDefault();
 
-        // Add your JavaScript code for calculations here
+        const tradeInputs = Array.from(tradesInput.querySelectorAll('tr'));
+        const trades = tradeInputs.map((row) => {
+            const ticker = row.querySelector('[name="ticker"]').value;
+            const entry = parseFloat(row.querySelector('[name="entry"]').value);
+            const tp = parseFloat(row.querySelector('[name="tp"]').value);
+            const sl = parseFloat(row.querySelector('[name="sl"]').value);
+            const winProbability = parseFloat(row.querySelector('[name="winProbability"]').value);
 
-        // Example:
-        // results.innerHTML = `
-        //     <tr>
-        //         <td>Ticker</td>
-        //         <td>Adjusted Trading Size</td>
-        //     </tr>
-        // `;
+            const rr = calculate_rr(entry, tp, sl);
+            const kellyValue = kelly_criterion(winProbability, rr);
+
+            return { ticker, entry, tp, sl, winProbability, rr, kellyValue };
+        });
+
+        const totalKellyValue = trades.reduce((sum, trade) => sum + trade.kellyValue, 0);
+
+        const tradingStack = parseFloat(prompt('Enter your trading stack:'));
+
+        trades.forEach((trade) => {
+            trade.adjustedTradingSize = trade.kellyValue * tradingStack / totalKellyValue;
+        });
+
+        results.innerHTML = trades
+            .map(
+                (trade) => `
+            <tr>
+                <td>${trade.ticker}</td>
+                <td>${trade.adjustedTradingSize.toFixed(2)}</td>
+            </tr>
+        `
+            )
+            .join('');
     });
 });
