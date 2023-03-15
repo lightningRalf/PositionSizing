97090a463dd1d8c88a88cc48fa37d6805305ff53
lightningRalf <126403501+lightningRalf@users.noreply.github.com>
2023-03-15 11:43:53 +0100

    Update script.js
    something went really wrong here. Now I dont see my first row for entry anymore, and the tab goes to the explorer menu.

--- script.js
+++ script.js
@@ -1,69 +1,76 @@
-function createRow() {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-        <td><input type="text" name="ticker" required></td>
-        <td><input type="number" step="0.01" name="entry" required></td>
-        <td><input type="number" step="0.01" name="tp" required></td>
-        <td><input type="number" step="0.01" name="sl" required></td>
-        <td><input type="number" step="0.01" min="0" max="1" name="winProbability" required></td>
-    `;
-    return row;
-}
-
-function calculate_rr(entry, tp, sl) {
-    return Math.abs((tp - entry) / (entry - sl));
-}
-
-function kelly_criterion(win_probability, win_loss_ratio) {
-    return win_probability - ((1 - win_probability) / win_loss_ratio);
-}
-
 document.addEventListener('DOMContentLoaded', () => {
+    const themeSwitch = document.getElementById('theme-switch');
     const tradesForm = document.getElementById('trades-form');
     const tradesInput = document.getElementById('trades-input');
-    const results = document.getElementById('results');
-    const addRowButton = document.getElementById('add-row');
+    const resultsTable = document.getElementById('results');
 
-    addRowButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        tradesInput.appendChild(createRow());
+    themeSwitch.addEventListener('change', () => {
+        document.body.classList.toggle('dark-mode');
     });
+
+    document.getElementById('trading-stack').focus();
+    addRow();
 
     tradesForm.addEventListener('submit', (event) => {
         event.preventDefault();
-
-        const tradeInputs = Array.from(tradesInput.querySelectorAll('tr'));
-        const trades = tradeInputs.map((row) => {
-            const ticker = row.querySelector('[name="ticker"]').value;
-            const entry = parseFloat(row.querySelector('[name="entry"]').value);
-            const tp = parseFloat(row.querySelector('[name="tp"]').value);
-            const sl = parseFloat(row.querySelector('[name="sl"]').value);
-            const winProbability = parseFloat(row.querySelector('[name="winProbability"]').value);
-
-            const rr = calculate_rr(entry, tp, sl);
-            const kellyValue = kelly_criterion(winProbability, rr);
-
-            return { ticker, entry, tp, sl, winProbability, rr, kellyValue };
-        });
-
-        const totalKellyValue = trades.reduce((sum, trade) => sum + trade.kellyValue, 0);
+        
+        tradesInput.classList.add('hidden');
+        resultsTable.classList.remove('hidden');
 
         const tradingStack = parseFloat(document.getElementById('trading-stack').value);
+        const trades = Array.from(tradesInput.rows)
+            .map(row => {
+                const inputs = row.querySelectorAll('input');
+                const ticker = inputs[0].value;
+                const entry = parseFloat(inputs[1].value);
+                const tp = parseFloat(inputs[2].value);
+                const sl = parseFloat(inputs[3].value);
+                const winProbability = parseFloat(inputs[4].value);
 
+                const rr = Math.abs((tp - entry) / (sl - entry));
+                const kellyFraction = (winProbability * rr - (1 - winProbability)) / rr;
+                const tradeSize = tradingStack * kellyFraction;
 
-        trades.forEach((trade) => {
-            trade.adjustedTradingSize = (trade.kellyValue / totalKellyValue) * tradingStack;
+                return { ticker, tradeSize };
+            });
+
+        trades.forEach((trade, index) => {
+            const row = resultsTable.insertRow();
+            const tickerCell = row.insertCell();
+            const tradeSizeCell = row.insertCell();
+            tickerCell.textContent = trade.ticker;
+            tradeSizeCell.textContent = trade.tradeSize.toFixed(2);
         });
-
-        results.innerHTML = trades
-            .map(
-                (trade) => `
-            <tr>
-                <td>${trade.ticker}</td>
-                <td>${trade.adjustedTradingSize.toFixed(2)}</td>
-            </tr>
-        `
-            )
-            .join('');
     });
 });
+
+function addRow() {
+    const row = tradesInput.insertRow();
+    for (let i = 0; i < 5; i++) {
+        const cell = row.insertCell();
+        const input = document.createElement('input');
+        input.type = i === 0 ? 'text' : 'number';
+        input.required = true;
+        input.tabIndex = i + 1;
+        cell.appendChild(input);
+    }
+
+    const winProbabilityInput = row.querySelector('input:nth-child(5)');
+    winProbabilityInput.addEventListener('focus', () => {
+        addRow();
+    });
+
+    winProbabilityInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            addRow();
+        }
+    });
+
+    winProbabilityInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            tradesForm.submit();
+        }
+    });
+}
